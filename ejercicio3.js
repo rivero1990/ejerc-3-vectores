4621278c0fae709fb0025b1d01a25178f400495d
Anibal Rivero <riveroleonel@gmail.com>
2023-05-24 19:46:48 -0300

    ejercicio 4 vectores

---
+++ ejercicio3.js
@@ -0,0 +1,11 @@
+const comidas = ["pizza", "hamburguesa", "empanada"];
+const precios = [54.5, 70.7, 54.8];
+const booleanos = [true, false, true];
+
+comidas.splice(1, 1);
+precios.splice(1, 1);
+booleanos.splice(1, 1);
+
+console.log(comidas);
+console.log(precios);
+console.log(booleanos);
